ac908084e5e9d593e6e12cb3827c1738ab124380
Yi9898 <yihuang9898@gmail.com>
2022-03-21 09:31:54 +0000

    Create codons

---
+++ R/data.R
@@ -0,0 +1,8 @@
+#' A standard codon table.
+#'
+#' This table contains all the codons and their corresponding amino acids.
+#'
+#' @format
+#'
+#' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
+"codons"
